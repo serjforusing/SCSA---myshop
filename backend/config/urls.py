5801bb58a79f65f6d996cfec46e993ae0bf94fc2
serjforusing <serjforusing@users.noreply.github.com>
2026-09-14 16:10:58 +0400

    გასწორება რენდერისთვისსსსს

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import RedirectView
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
+    path("", RedirectView.as_view(pattern_name="swagger-ui", permanent=False)),
     path("admin/", admin.site.urls),
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path("api/docs/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
